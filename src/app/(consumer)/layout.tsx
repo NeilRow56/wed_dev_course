d4640b795f6c23a9621e74ecec05d5331c5c05a4
NeilRow56 <neilrowland56@gmail.com>
2025-04-23 15:56:05 +0100

    Added Clerk authentication and consumer group with specific layout

---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,63 @@
+import { Button } from '@/components/ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import { ReactNode, Suspense } from 'react'
+
+export default function ConsumerLayout({
+  children
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
+}
+
+function Navbar() {
+  return (
+    <header className='bg-background z-10 flex h-12 shadow'>
+      <nav className='container mx-auto flex gap-4 px-8'>
+        <Link
+          className='mr-auto flex items-center text-lg hover:underline'
+          href='/'
+        >
+          Web Dev Simplified
+        </Link>
+        <Suspense>
+          <SignedIn>
+            Admin Link
+            <Link
+              className='hover:bg-accent/10 flex items-center px-2'
+              href='/courses'
+            >
+              My Courses
+            </Link>
+            <Link
+              className='hover:bg-accent/10 flex items-center px-2'
+              href='/purchases'
+            >
+              Purchase History
+            </Link>
+            <div className='size-8 self-center'>
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: { width: '100%', height: '100%' }
+                  }
+                }}
+              />
+            </div>
+          </SignedIn>
+        </Suspense>
+        <Suspense>
+          <SignedOut>
+            <Button className='self-center' asChild>
+              <SignInButton>Sign In</SignInButton>
+            </Button>
+          </SignedOut>
+        </Suspense>
+      </nav>
+    </header>
+  )
+}
