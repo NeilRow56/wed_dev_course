557ef3e8922c8b8a7b5633875f23be168be8a81d
NeilRow56 <neilrowland56@gmail.com>
2025-04-23 12:13:18 +0100

    Drizzle schemas added and database up and running with all tables

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    dynamicIO: true,
+    authInterrupts: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hie937imukqznlzt.public.blob.vercel-storage.com",
+      },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "static.vecteezy.com" },
+      { protocol: "https", hostname: "avatar.vercel.sh" },
+      { protocol: "https", hostname: "utfs.io" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+    ],
+  },
 };
 
 export default nextConfig;
